d154c7ad1809f82538394cf5192199ffad40f541
KateIsabelle <kate.davis887@gmail.com>
2021-03-10 18:53:16 +0000

    update client to write my name to the server

--- client.js
+++ client.js
@@ -16,20 +16,23 @@
 //   client.write(input);
 // })
 
-const net = require('net');
+const net = require("net");
 
 //Establishes connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.222.131',
+    host: "135.23.222.131",
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8');
+  conn.setEncoding("utf8");
   conn.on("data", (data) => {
-    console.log("data came back from server...");
     console.log(data);
-  })
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  conn.write("Name: KID");
   return conn;
 }
 
